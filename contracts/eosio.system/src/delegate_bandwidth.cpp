004c7b00a6b1d82c5b3e81c43228437d867e66d9
Emil Guseynov <theluckyemil@gmail.com>
2019-09-25 18:22:49 +0300

    check unstake_qty is positive

--- contracts/eosio.system/src/delegate_bandwidth.cpp
+++ contracts/eosio.system/src/delegate_bandwidth.cpp
@@ -483,7 +483,8 @@ namespace eosiosystem {
       asset zero_asset( 0, core_symbol() );
       check( unstake_cpu_quantity >= zero_asset, "must unstake a positive amount" );
       check( unstake_net_quantity >= zero_asset, "must unstake a positive amount" );
-      check( unstake_cpu_quantity.amount + unstake_net_quantity.amount > 0, "must unstake a positive amount" );
+      check( unstake_vote_quantity >= zero_asset, "must unstake a positive amount" );
+      check( unstake_cpu_quantity.amount + unstake_net_quantity.amount + unstake_vote_quantity.amount > 0, "must unstake a positive amount" );
       check( _gstate.total_activated_stake >= min_activated_stake,
              "cannot undelegate bandwidth until the chain is activated (at least 15% of all tokens participate in voting)" );
 
