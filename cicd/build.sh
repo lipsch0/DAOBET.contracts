9fd8a912a2bfce399dd8a636d7356ee69cd47507
Andrey Levkin <alevkin@gmail.com>
2019-10-14 12:19:10 +0300

    Added creating assets for GitHub Releases and deploy (#7)
    * Added creating assets for GitHub Releases and deploy
    
    * Fix version format
    
    * Fixed LLVM bug with linker

--- cicd/build.sh
+++ cicd/build.sh
@@ -8,6 +8,6 @@ NC='\033[0m'
 CORES=`getconf _NPROCESSORS_ONLN`
 mkdir -p build
 pushd build &> /dev/null
-cmake ../ -DCMAKE_MODULE_PATH=/opt/eosio/lib/cmake/haya
+cmake ../ -DCMAKE_MODULE_PATH=/opt/eosio/lib/cmake/haya -DCMAKE_LINKER=/usr/opt/eosio.cdt/1.6.1/bin/lld
 make -j${CORES}
 popd &> /dev/null
